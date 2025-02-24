4b47dc9c897d73710b6cc0984c1fa4c2a2984c01
ruslanwahyudi <ruslanwahyudi1@gmail.com>
2025-02-24 13:23:18 +0700

    upd fcm

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -123,10 +123,10 @@ class NotificationController extends Controller
             $client->setAuthConfig([
                 "type" => "service_account",
                 "project_id" => config('services.firebase.project_id'),
-                "private_key_id" => "private_key_id",
+                "private_key_id" => config('services.firebase.private_key_id'),
                 "private_key" => config('services.firebase.private_key'),
                 "client_email" => config('services.firebase.client_email'),
-                "client_id" => "client_id",
+                "client_id" => config('services.firebase.client_id'),
                 "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
                 "token_uri" => "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
